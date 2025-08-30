7a1a4c16d938f889f1c3326d6f8ff7cf2f74b495
Andres Verona <saqv2002@gmail.com>
2025-08-30 02:22:28 -0500

    nuevo commit

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
 import sys 
 import getpass
 
-usr_ = "admin"
-pwd_ = 123
-print(f)
-print("BIENVENIDO AL SPA ROBLES")
+def validation():
+        pwd_ = 123
+        print(f"Bienvenido Admin")
+
+        pwd = getpass.getpass("Escribe la contraseña correcta: ")
+
+        if pwd_==pwd:
+            print("BIENVENIDO AL SPA ROBLES")
+        
+
+        return
+contra = validation()
+print(contra)
+
+
+
+
 print("ESTA ES LA CALCULADORA PARA LOS PRECIOS")
-print("PORFAVOR, PROPORCIONA TUS SIGUIENTES DATOS")
+#print("PORFAVOR, PROPORCIONA TUS SIGUIENTES DATOS")
 
