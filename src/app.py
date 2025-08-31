379072c3ef2155b01bd8baa08d310c2029155cd1
Andres Verona <saqv2002@gmail.com>
2025-08-31 03:47:00 -0500

    ultimo commit de la noche, bloqueo mental jajajja

--- src/app.py
+++ src/app.py
@@ -14,10 +14,15 @@ def validation():
 
             if pwd==pwd_:
                 print("CONTRASEÑA CORRECTA")
+                print()
+                print()
                 return True
+                
             else:
                 print("CONTRASEÑA INCORRECTA")
                 intentos += 1
+            print()
+            print()
 #la siguiente función nos ayudará a cerrar el programa una vez se hayan errado las tres oportunidades para ingresar la contraseña
 #he creado un else por fuera de el nivel del if para que el programa no se cierre con el primer incremento a la variable intentos
                 #sys.exit()
@@ -34,11 +39,37 @@ print(contra)
 def datosCliente():
      Nombre=input("Porfavor ingresa tu nombre: ")
      Cc = input("Porfavor ingresa tu C.C.: ")
-     estrato=input("Porfavor ingresa tu estrato socioeconomico")
-     servicioSpa=input("in")
+     estrato=input("Porfavor ingresa tu estrato socioeconomico: ")
+     print()
+     print()
      return
 dclient=datosCliente()
 print(dclient)
+
+def serviciosSpa():
+       
+       tabladeprecios={opcion1}
+       
+       opcion1=int(60000) 
+       opcion2=int(90000) 
+       opcion3=int(100000) 
+       opcion4=int(140000)  
+       servicios=[
+            "1. Corte y cepillado: $60.000",
+            "2. Corte, cepillado y uñas: $90.000", 
+            "3. Uñas en acrílico y cejas: $100.000", 
+            "4. Uñas en acrílico, maquillaje y cejas: $ 140.000"  
+
+       ]
+       print("\n".join(servicios))
+       OpCli=input("¿Que servicio deseas? Selecciona el número del servicio: ")
+       
+
+
+       #print(opcion1,opcion2,opcion3,opcion4)
+
+serspa=serviciosSpa()
+print(serspa)
 
 
 print("ESTA ES LA CALCULADORA PARA LOS PRECIOS")
