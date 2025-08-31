e29abc839d5e4fe38d6446e99840a44aae702a0f
Andres Verona <saqv2002@gmail.com>
2025-08-30 23:56:40 -0500

    nuevo commit

--- src/app.py
+++ src/app.py
@@ -2,14 +2,22 @@ import sys
 import getpass
 
 def validation():
-        pwd_ = 123
-        print(f"Bienvenido Admin")
-
-        pwd = getpass.getpass("Escribe la contraseña correcta: ")
-
-        if pwd_==pwd:
-            print("BIENVENIDO AL SPA ROBLES")
+        pwd_ = "123"
+        intentos = 0
+        maxIntentos = 3
         
+        while intentos < maxIntentos:
+            print(f"Bienvenido Admin")
+
+            pwd = getpass.getpass("Escribe la contraseña correcta: ")
+
+            if pwd==pwd_:
+                print("CONTRASEÑA CORRECTA")
+                return True
+            else:
+                print("CCONTRASEÑA INCORRECTA")
+                intentos += 1
+            
 
         return
 contra = validation()
