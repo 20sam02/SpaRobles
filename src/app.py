251557ddf1b299b8b7b999ccd5ea0cfb927d63a1
Andres Verona <saqv2002@gmail.com>
2025-09-01 22:52:40 -0500

    último commit pero a qué costo?

--- src/app.py
+++ src/app.py
@@ -10,10 +10,10 @@ def validation():
         while intentos < maxIntentos:
             print(f"Bienvenido Admin")
 
-            pwd = getpass.getpass("Escribe la contraseña correcta: ")
+            pwd = getpass.getpass(f"Escribe la contraseña correcta: ")
 
             if pwd==pwd_:
-                print("CONTRASEÑA CORRECTA")
+                print(f"CONTRASEÑA CORRECTA")
                 print()
                 print()
                 return True
@@ -36,41 +36,71 @@ contra = validation()
 print(contra)
 
 #en esta segunda función pediremos al cliente que proporcione sus datos
-def datosCliente():
-     Nombre=input("Porfavor ingresa tu nombre: ")
-     Cc = input("Porfavor ingresa tu C.C.: ")
-     estrato=input("Porfavor ingresa tu estrato socioeconomico: ")
+def datosCliente(NombreCli=(), Cc=()):
+     NombreCli=input(f"Porfavor ingresa tu nombre: ")
+     Cc = input(f"Porfavor ingresa tu C.C.: ")
      print()
      print()
-     return
+     return NombreCli, Cc
+NombreCli, Cc = datosCliente()
 dclient=datosCliente()
 print(dclient)
 
 def serviciosSpa():
-       
-       tabladeprecios={opcion1}
-       
-       opcion1=int(60000) 
-       opcion2=int(90000) 
-       opcion3=int(100000) 
-       opcion4=int(140000)  
-       servicios=[
-            "1. Corte y cepillado: $60.000",
-            "2. Corte, cepillado y uñas: $90.000", 
-            "3. Uñas en acrílico y cejas: $100.000", 
-            "4. Uñas en acrílico, maquillaje y cejas: $ 140.000"  
+        #aquí creamos un diccionario donde tendremos una clave que será el número del 
+        # Servicio, el cual el cliente podrá elegir, seguido del nombre y el valor del servicio.
+    servicios={
+            1:("Corte y cepillado:", 60000),
+            2: ("Corte, cepillado y uñas:", 90000), 
+            3:("Uñas en acrílico y cejas:", 100000), 
+            4: ("Uñas en acrílico, maquillaje y cejas:", 140000)  
 
-       ]
-       print("\n".join(servicios))
-       OpCli=input("¿Que servicio deseas? Selecciona el número del servicio: ")
-       
+            }
+       #en este punto mostramos por consola el listado de precios y servici
+       # luego
+    #print("\n".join(servicios))
+    print(servicios)
 
 
-       #print(opcion1,opcion2,opcion3,opcion4)
+    OpCli=int(input("¿Que servicio deseas? Selecciona el número del servicio: "))
+    if OpCli in servicios:
+        nombre_servicio, precio = servicios[OpCli]
+        print(f"\n Servicio seleccionado: {nombre_servicio} - ${precio}")
 
+        # Ahora pedimos el estrato
+        estrato = int(input("Ingrese su estrato socioeconómico: "))
+
+        # con este bloque calculamos el descuento dependiendo del estrato que el cliente proporcionó
+        if estrato in [1, 2]:
+            descuento = 0.15
+        elif estrato in [3, 4]:
+            descuento = 0.10
+        else:
+            descuento = 0.05
+
+        valor_final = precio - (precio * descuento)
+        return precio, descuento, valor_final
+        
+
+        #print(f"Nombre del cliente: {NombreCli}")
+        #print(f"Cédula del cliente: {Cc}")
+        #print(f" Precio original: ${precio}")
+        #print(f" Descuento aplicado: {descuento*100}%")
+        #print(f" Valor final a pagar: ${valor_final}")
+#con esta parte del condicional, le arrojamos un mensaje al usuario, si ingresa un valor diferente a los servicios ofertados
+    else:
+        print("❌ Esa opción no existe en el menú")
+precio, descuento, valor_final =serviciosSpa()
+#prueba = datosCliente()
+#serviciosSpa(prueba)
 serspa=serviciosSpa()
 print(serspa)
 
+print(f"Nombre del cliente: {NombreCli}")
+print(f"Cédula del cliente: {Cc}")
+print(f" Precio original: ${precio}")
+print(f" Descuento aplicado: {descuento*100}%")
+print(f" Valor final a pagar: ${valor_final}")
 
 print("ESTA ES LA CALCULADORA PARA LOS PRECIOS")
 #print("PORFAVOR, PROPORCIONA TUS SIGUIENTES DATOS")
